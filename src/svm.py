07922713b00a99d156febf26a9ea85abc7b5269f
Pedro Albuquerque <pedro.albuquerque.1396333@sga.pucminas.br>
2024-11-22 20:45:19 -0300

    fix: svm class input

--- src/svm.py
+++ src/svm.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import scipy
+import csv
 from sklearn.svm import SVC
 from sklearn.calibration import cross_val_predict
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -20,15 +21,22 @@ images = data_array["images"]
 classes = data_array["class"]
 fat = data_array["fat"]
 
+HEALTHY = "Paciente Saudável"
+FATTY = "Paciente com Esteatose"
+
 X = []
 y = []
 groups = []
 
-for i in range(len(images[0])):
-    for j in range(len(images[0][i])):
-        X.append(images[0][i][j])
-        y.append(int(classes[0][i][0][0]))
-        groups.append(i)
+with open("src/dados_classificador.csv", newline="") as csvfile:
+    reader=csv.reader(csvfile)
+    rows=[r for r in reader]
+    for i in range(len(images[0])):
+        state = 0 if rows[i][1] == HEALTHY else 1
+        for j in range(len(images[0][i])):
+            X.append(images[0][i][j])
+            y.append(state)
+            groups.append(i)
 
 logo = LeaveOneGroupOut()
 
@@ -55,3 +63,5 @@ print(f"Sensitivity (Recall for Positive Class): {sensitivity:.2f}")
 print(f"Specificity (Recall for Negative Class): {specificity:.2f}")
 print("\nConfusion Matrix:")
 print(cm)
+
+
