21cb6633c4f0891c049ffc9cb2289be627f60531
Pedro Albuquerque <pedro.albuquerque.1396333@sga.pucminas.br>
2024-11-26 07:17:15 -0300

    feat: mobilenet

--- src/mobilenet.py
+++ src/mobilenet.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from PIL import Image
 
 from cv2 import imread
+from matplotlib import pyplot as plt
 import numpy as np
 from keras.src.applications.mobilenet_v2 import preprocess_input
 from keras.src.applications.mobilenet_v2 import MobileNetV2
@@ -12,37 +13,46 @@ from keras.src.losses import sparse_categorical_crossentropy
 from keras.src.optimizers import Adam
 import tensorflow as tf
 from keras.src.callbacks import CSVLogger
-
-
+from keras.src.layers import Dropout
+from keras.src.layers import Dense
+from keras.src.models import Model
+from keras.src.layers import Conv2D
+from keras.src.layers import MaxPooling2D
+from keras.src.layers import Flatten
+from keras.src.layers import BatchNormalization
+from keras.src.regularizers import L2
 HEALTHY = "Paciente Saudável"
+
+
+
 path_input_dir = Path("images")
 X = []
 y = []
 groups = []
-
+csvIndex = 1
 with open("src/dados_classificador.csv", newline="") as csvfile:
     reader=csv.reader(csvfile)
     rows=[r for r in reader]
     for index, path in enumerate(sorted(path_input_dir.iterdir(), key=lambda m: int(m.name.split('_')[-1]))):
-        state = 0 if rows[index][1] == HEALTHY else 1
         for imagePath in path.iterdir():
+            state = 0 if rows[csvIndex][1] == HEALTHY else 1
             image = Image.open(imagePath).resize((224, 224)).convert('RGB')
             image = np.array(image)
             X.append(image)
             y.append(state)
             groups.append(index)
+            csvIndex += 1
 
 X = np.array(X)
 
-
 logo = LeaveOneGroupOut()
-csv_logger = CSVLogger("model_history_log.csv", append=True)
 
 acc_per_fold = []
 loss_per_fold = []
-all_true_labels = []
-all_pred_labels = []
+conf_matrices = []
 for i, (train_index, test_index) in enumerate(logo.split(X, y, groups)):
+    path = "mobileNetHistory/logs/model_history_log_fold_" + str(i) + ".csv"
+    csv_logger = CSVLogger(path, append=True)
     X_train = []
     y_train = []
     X_test = []
@@ -57,8 +67,19 @@ for i, (train_index, test_index) in enumerate(logo.split(X, y, groups)):
     y_train = np.array(y_train)
     X_test = np.array(X_test)
     y_test = np.array(y_test)
-    model = MobileNetV2(weights='imagenet')
-    model.compile(loss=sparse_categorical_crossentropy, optimizer=Adam(), metrics=['accuracy'])
+    base_model = MobileNetV2(weights='imagenet',include_top=False)
+    for layer in base_model.layers[:120]:  # Congele as 100 primeiras camadas
+        layer.trainable = False 
+    model = base_model.output 
+    model = tf.keras.layers.GlobalAveragePooling2D()(model)
+    model = BatchNormalization()(model)
+    model = Dense(128, activation='relu', kernel_regularizer=L2(0.001))(model)
+    model = Dropout(0.5)(model)
+    model = Dense(64, activation='relu', kernel_regularizer=L2(0.001))(model)
+    model = Dropout(0.5)(model)
+    output = Dense(2, activation='softmax')(model)
+    model = Model(inputs=base_model.input, outputs=output)
+    model.compile(loss=sparse_categorical_crossentropy, optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
 
     # Generate a print
     print('------------------------------------------------------------------------')
@@ -67,23 +88,39 @@ for i, (train_index, test_index) in enumerate(logo.split(X, y, groups)):
     
     # Fit data to model
     X_train = preprocess_input(X_train)
+    X_test = preprocess_input(X_test)
     history = model.fit(X_train, y_train,
-                batch_size=50,
+                batch_size=32,
                 epochs=20,
-                verbose=1, callbacks=[csv_logger])
+                verbose=1, callbacks=[csv_logger], validation_data=(X_test, y_test), )
 
     # Generate generalization metrics
-    X_test = preprocess_input(X_test)
     scores = model.evaluate(X_test, y_test, verbose=0)
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
 
     # Get predictions and convert to class labels
     y_pred = model.predict(X_test)
-    y_pred_classes = np.argmax(y_pred, axis=1)  # Convert probabilities to class labels
-    y_true_classes = np.argmax(y_test, axis=1)  # Convert one-hot encoded to class labels
-    
-    # Append true and predicted labels
-    all_true_labels.extend(y_true_classes)
-    all_pred_labels.extend(y_pred_classes)
+    y_pred_classes = np.argmax(y_pred, axis=1)
+    cm = confusion_matrix(y_test, y_pred_classes)
+    conf_matrices.append(cm)
+
+    plt.clf()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig("mobileNetHistory/graphs/loss_fold_" + str(i) + ".png")
+    plt.clf()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig("mobileNetHistory/graphs/acc_fold_" + str(i) + ".png")
 
 
 # == Provide average scores ==
@@ -97,6 +134,6 @@ print('Average scores for all folds:')
 print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
 print(f'> Loss: {np.mean(loss_per_fold)}')
 print('------------------------------------------------------------------------')
-conf_matrix = confusion_matrix(all_true_labels, all_pred_labels)
-print('Confusion Matrix:')
-print(conf_matrix)
+avg_conf_matrix = np.sum(conf_matrices, axis=0)
+print("Average Confusion Matrix:")
+print(avg_conf_matrix)
