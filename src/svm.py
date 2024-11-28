ff374bf303b9b0c7bab6af94eaa6cfa3a4b7db1c
Pedro Albuquerque <pedro.albuquerque.1396333@sga.pucminas.br>
2024-11-27 22:35:51 -0300

    feat: serialized svm

--- src/svm.py
+++ src/svm.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pickle
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -7,57 +8,62 @@ import csv
 from sklearn.svm import SVC
 from sklearn.calibration import cross_val_predict
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.model_selection import LeaveOneGroupOut, train_test_split
 from PIL import Image
 
 
-path_input_dir = Path("data")
-
-path_data = (
-                path_input_dir / "dataset_liver_bmodes_steatosis_assessment_IJCARS.mat"
-            )
-
-data = scipy.io.loadmat(str(path_data))
-data_array = data["data"]
-images = data_array["images"]
-classes = data_array["class"]
-fat = data_array["fat"]
-
 HEALTHY = "Paciente Saudável"
 
+path_input_dir = Path("images")
 X = []
-y = []  
+y = []
 groups = []
-
-with open("src/dados_classificador.csv", newline="") as csvfile:
+csvIndex = 1
+with open("src/dados_hu.csv", newline="") as csvfile:
     reader=csv.reader(csvfile)
     rows=[r for r in reader]
-    for i in range(len(images[0])):
-        state = 0 if rows[i][1] == HEALTHY else 1
-        for j in range(len(images[0][i])):
-            X.append(images[0][i][j])
+    for index, path in enumerate(sorted(path_input_dir.iterdir(), key=lambda m: int(m.name.split('_')[-1]))):
+        for imagePath in path.iterdir():
+            state = 0 if rows[csvIndex][1] == HEALTHY else 1
+            image = Image.open(imagePath).resize((224, 224)).convert('RGB')
+            image = np.array(image)
+            X.append(image)
             y.append(state)
-            groups.append(i)
-
-logo = LeaveOneGroupOut()
+            groups.append(index)
+            csvIndex += 1
 
 X = np.array(X)
+y = np.array(y)
 X = X.reshape(X.shape[0], -1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 model = SVC(kernel='linear', random_state=42)
 
-y_pred = cross_val_predict(model, X, y, cv=logo, groups=groups)
+model.fit(X_train, y_train)
 
-cm = confusion_matrix(y, y_pred, labels=[0, 1])
-tn, fp, fn, tp = cm.ravel()
+file = open("svmModel/svm_model.pkl", "wb")
+pickle.dump(model, file)
 
-accuracy = accuracy_score(y, y_pred)
-sensitivity = tp / (tp + fn) 
-specificity = tn / (tn + fp) 
+# logo = LeaveOneGroupOut()
 
-print("Shallow Classifier")
-print(f"Accuracy: {accuracy:.2f}")
-print(f"Sensitivity (Recall for Positive Class): {sensitivity:.2f}")
-print(f"Specificity (Recall for Negative Class): {specificity:.2f}")
-print("\nConfusion Matrix:")
-print(cm)
+# X = np.array(X)
+# X = X.reshape(X.shape[0], -1)
+
+# model = SVC(kernel='linear', random_state=42)
+
+# y_pred = cross_val_predict(model, X, y, cv=logo, groups=groups)
+
+# cm = confusion_matrix(y, y_pred, labels=[0, 1])
+# tn, fp, fn, tp = cm.ravel()
+
+# accuracy = accuracy_score(y, y_pred)
+# sensitivity = tp / (tp + fn) 
+# specificity = tn / (tn + fp) 
+
+# print("Shallow Classifier")
+# print(f"Accuracy: {accuracy:.2f}")
+# print(f"Sensitivity (Recall for Positive Class): {sensitivity:.2f}")
+# print(f"Specificity (Recall for Negative Class): {specificity:.2f}")
+# print("\nConfusion Matrix:")
+# print(cm)
